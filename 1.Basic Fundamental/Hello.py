32e7a891dade23acbe3df42ee4ff2bc63b28edf9
mtekateka <39678622+mtekateka@users.noreply.github.com>
2018-08-07 00:43:11 +0300

    Update Hello.py
    use %s for passing string

--- 1.Basic Fundamental/Hello.py
+++ 1.Basic Fundamental/Hello.py
@@ -8,4 +8,9 @@ print('Hello World')
 #To comment in Python we use hash 
 print('To comment on Python we use # sign for the Single line of code') 
 
+#Also there a different ways to print like
+
+print("hello python : {} ".format(x_variable))
+
+print("hello python : %s %s " %(x_variable,y_variable))
 
